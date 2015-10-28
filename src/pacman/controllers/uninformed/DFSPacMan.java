6df53ba7c331fbad02c8c3442c687cecacf8c793
Whitney Mulhern <Whitney@172-16-239-179.DYNAPOOL.NYU.EDU>
2015-10-28 19:21:30 -0400

    fixed basic DFS

--- src/pacman/controllers/uninformed/DFSPacMan.java
+++ src/pacman/controllers/uninformed/DFSPacMan.java
@@ -3,34 +3,44 @@ package pacman.controllers.uninformed;
 import java.util.ArrayList;
 import pacman.controllers.Controller;
 import pacman.game.Game;
-
+import java.util.EnumMap;
 import static pacman.game.Constants.*;
 
 /*
  */
 public class DFSPacMan extends Controller<MOVE> {	
 	
-	Controller<EnumMap<Constants.GHOST, MOVE>> spookies;
-	public DFSPacMan(Controller<EnumMap<Constants.GHOST, MOVE>> spookies){
+	Controller<EnumMap<GHOST, MOVE>> spookies;
+	public DFSPacMan(Controller<EnumMap<GHOST, MOVE>> spookies){
 		this.spookies = spookies;
 	}
-	public MOVE getMove(Game game,long timeDue) {			
+	public MOVE getMove(Game game,long timeDue) {
 		Game[] games = new Game[5];
-		MOVE[] moves = MOVES.values();
-		int maxScore = 0;
+		MOVE[] moves = MOVE.values();
+		int maxScore = 0;//game.getScore();
 		int bestMove = 0;
+		int i = 0;
 		for (MOVE move : MOVE.values()){
 			moves[i] = move;
+			i++;
 		}
-		for(int i = 0; i < 5; i++){
+		int[] scores = new int[5];
+		for(i = 0; i < 5; i++) {
 			games[i] = game.copy();
-			games[i].advanceGame(moves[i], spookies.getMove(games[i], -1));
-			if (games[i].getScore() > maxScore){
-				maxScore = games[i].getScore();
-				bestMove = i;
+			for (int j = 0; j < 4; j++) {
+				games[i].advanceGame(moves[i], spookies.getMove(games[i], -1));
+			}
+			scores[i] = games[i].getScore();
+		}
+
+		System.out.println();
+		for (int j = 0; j < 5; j++){
+			if (scores[j] > maxScore){
+				maxScore = scores[j];
+				bestMove = j;
 			}
 		}
-		return moves[i];
+		return moves[bestMove];
 	}
 }
 
