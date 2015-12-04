a7c0b1c3f9fa52d36f46200039a98f180b178082
Giorgio <gp922@nyu.edu>
2015-12-04 03:44:03 -0500

    the uninformed search overhaul of the century

--- src/pacman/controllers/uninformed/BFS.java
+++ src/pacman/controllers/uninformed/BFS.java
@@ -1,53 +1,58 @@
 package pacman.controllers.uninformed;
 
+import apple.laf.JRSUIUtils;
 import pacman.controllers.Controller;
 import pacman.game.Game;
 import java.util.EnumMap;
+import java.util.PriorityQueue;
 import java.util.Stack;
 import java.util.ArrayList;
 import static pacman.game.Constants.*;
 
 /**
- * A simple depth first search for PacMan
+ * A simple breadth first search for PacMan
  */
-public class DFS extends Controller<MOVE> {
+public class BFS extends Controller<MOVE> {
     private static int ITERATIONS = 3;
-    private Stack<TreeNode> tree = new Stack<>();
+    private PriorityQueue<TreeNode> tree = new PriorityQueue<>();
+    private ArrayList<TreeNode> treeIndexed = new ArrayList<>();
 
 
     Controller<EnumMap<GHOST, MOVE>> spookies;
-    public DFS(Controller<EnumMap<GHOST, MOVE>> spookies) {
+    public BFS(Controller<EnumMap<GHOST, MOVE>> spookies) {
         this.spookies = spookies;
     }
 
     /**
-     * Performs depth first search to a depth of 3, before timing out. Returns the parent of the deepest explored
+     * Performs breadth first search to a depth of 3, before timing out. Returns the parent of the deepest explored
      * move.
      * @param game A copy of the current game
      * @param timeDue The time the next move is due
      * @return move decision
      */
+
     public MOVE getMove(Game game,long timeDue) {
         int iter = ITERATIONS;
 
         //root node
-        tree.push(new TreeNode(game.copy(), null, null, 0));
-
-        while(iter > 0){
-            TreeNode tmp = tree.peek();
-            for (MOVE move : MOVE.values()){
-                Game tmpGame = tmp.getGame().copy();
-                for(int i = 0; i < 4; i++){
+        TreeNode root = new TreeNode(game.copy(), null, null, 0);
+        tree.add(root);
+        treeIndexed.add(root);
+        for(int i = 0; i < iter; i++){
+            TreeNode tmp = treeIndexed.get(i);
+            for(MOVE move : MOVE.values()){
+                Game tmpGame = tmp.getGame();
+                for(int j = 0; j < 4; j++){
                     tmpGame.advanceGame(move, spookies.getMove(tmpGame.copy(), -1));
                 }
-
                 TreeNode child = new TreeNode(tmpGame, move, tmp, tmp.getDepth() + 1);
-                tree.push(child);
+                tree.add(child);
+                treeIndexed.add(child);
             }
-            iter--;
         }
 
-        TreeNode foundMove = tree.pop();
+        tree.remove(); // get rid of root node
+        TreeNode foundMove = tree.remove();
         while(foundMove.getDepth() != 1){
             foundMove = foundMove.getParent();
         }
--- src/pacman/controllers/uninformed/TreeNode.java
+++ src/pacman/controllers/uninformed/TreeNode.java
@@ -2,31 +2,39 @@ package pacman.controllers.uninformed;
 import apple.laf.JRSUIUtils;
 import pacman.controllers.Controller;
 import pacman.game.Game;
+
+import java.util.Comparator;
 import java.util.EnumMap;
 import static pacman.game.Constants.*;
 
 /**
- * A simple tree node implementation for use in uninformed search algorithms. Keeps track of game and move.
+ * A simple tree node implementation for use in uninformed search algorithms. Keeps track of game, move and depth.
  */
-public class TreeNode {
+public class TreeNode implements Comparable<TreeNode> {
     private Game game;
     private MOVE move;
     private TreeNode parent;
+    private int depth;
 
     /**
      * Tree Node constructor
      * @param game Game to be simulated
      * @param move Move played in game
+     * @param depth Depth of node in tree
      */
 
-    public TreeNode(Game game, MOVE move, TreeNode parent){
+    public TreeNode(Game game, MOVE move, TreeNode parent, int depth){
         this.game = game;
         this.move = move;
         this.parent = parent;
+        this.depth = depth;
     }
 
     // Setters and getters
 
+    public int compareTo(TreeNode other){
+        return Integer.compare(this.depth, other.depth);
+    }
     public Game getGame(){
         return game;
     }
@@ -36,6 +44,7 @@ public class TreeNode {
     public TreeNode getParent(){
         return parent;
     }
+    public int getDepth(){ return depth; }
 
     public void setGame(Game game){
         this.game = game;
@@ -46,5 +55,6 @@ public class TreeNode {
     public void setParent(TreeNode parent){
         this.parent = parent;
     }
+    public void setDepth(int depth){ this.depth = depth; }
 
 }
